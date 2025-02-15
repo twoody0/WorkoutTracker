e8b2d86110eec15cd8a78aea3a69564a1292c69b
Tyler Woody <104695415+twoody0@users.noreply.github.com>
2025-02-15 12:15:35 -0800

    Added a WorkoutType enum

--- Models/Workout.cs
+++ Models/Workout.cs
@@ -1,11 +1,26 @@
-﻿namespace WorkoutTracker.Models;
+﻿using System;
+
+namespace WorkoutTracker.Models;
+
+public enum WorkoutType
+{
+    WeightLifting,
+    Cardio
+}
 
 public class Workout
 {
-    public string Name { get; set; }       // Optional: name of the workout (e.g., "Bench Press")
+    public string Name { get; set; }
+    public WorkoutType Type { get; set; }
+
+    // For weight lifting workouts:
     public double Weight { get; set; }
     public int Reps { get; set; }
     public int Sets { get; set; }
+
+    // For cardio workouts:
+    public int Steps { get; set; }
+
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
 }
