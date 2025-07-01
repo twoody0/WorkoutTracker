44af91641149b1f256e040505983b37e6ac7e393
Windeko <tylerjameswoody@gmail.com>
2025-06-30 19:33:04 -0700

    Added defaults to silence compiler

--- WorkoutTracker.App/Models/User.cs
+++ WorkoutTracker.App/Models/User.cs
@@ -1,17 +1,20 @@
-﻿namespace WorkoutTracker.Models;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace WorkoutTracker.Models;
 
 public class User
 {
-    public required string Name { get; set; }
+    public required string Name { get; set; } = default!;
     public int Age { get; set; }
     public double Weight { get; set; }
-    public required string Username { get; set; }
-    public required string Password { get; set; }
-    public required string Email { get; set; }
+    public required string Username { get; set; } = default!;
+    public required string Password { get; set; } = default!;
+    public required string Email { get; set; } = default!;
 
-    // Required for data binding / deserialization
+    // Parameterless constructor for data binding / deserialization
     public User() { }
 
+    [SetsRequiredMembers]
     public User(string name, int age, double weight, string username, string password, string email)
     {
         Name = name;
--- WorkoutTracker.App/Models/Workout.cs
+++ WorkoutTracker.App/Models/Workout.cs
@@ -1,4 +1,6 @@
-﻿namespace WorkoutTracker.Models;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace WorkoutTracker.Models;
 
 public enum WorkoutType
 {
@@ -8,20 +10,21 @@ public enum WorkoutType
 
 public class Workout
 {
-    public required string Name { get; set; }
+    public required string Name { get; set; } = default!;
+    public required string MuscleGroup { get; set; } = default!;
+    public required string GymLocation { get; set; } = default!;
+
     public double Weight { get; set; }
     public int Reps { get; set; }
     public int Sets { get; set; }
-    public required string MuscleGroup { get; set; }
     public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; } 
-    public int Steps { get; set; } 
+    public DateTime EndTime { get; set; }
+    public int Steps { get; set; }
     public WorkoutType Type { get; set; }
-    public required string GymLocation { get; set; }
 
-    // Required for data binding / deserialization
     public Workout() { }
 
+    [SetsRequiredMembers]
     public Workout(string name, double weight, int reps, int sets, string muscleGroup, DateTime startTime, WorkoutType type, string gymLocation)
     {
         Name = name;
